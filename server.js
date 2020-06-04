08cf9823136785622175e3d9447165b0e5c4b10e
Grayson Deese <gldeese@ncsu.edu>
2020-06-03 20:52:11 -0400

    Setting up the home page.

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
+// Setting the home page to the path /
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+})
 // Setting up the notes variable by reading the db.json file
 let notes = fs.readFileSync(__dirname + '/db/db.json', 'utf-8', (err) => {
     if(err) {
